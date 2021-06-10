c846b1b8411eddcb7cf8069b6c85c951cb343958
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-06-10 00:26:11 -0400

    Revert "Remove /projectInfo"
    This reverts commit 308cc1cb72e97175853206803ffb37d56f738a30.

--- bin/serve.ts
+++ bin/serve.ts
@@ -2,13 +2,14 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 
-import { handleTransaction } from "../src";
+import { handleProjectInfo, handleTransaction } from "../src";
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.get("/projectInfo", handleProjectInfo);
 app.get("/tx", handleTransaction);
 console.log(`Starting on port ${process.env.PORT}`);
 
