82f27747a4b61c129b80b21f3fd94ac7da893e93
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-06-10 01:34:35 -0400

    Extract common handler setup

--- src/handleTransaction.ts
+++ src/handleTransaction.ts
@@ -5,42 +5,15 @@ import { forAddress, ProjectInfo, Transaction } from "@truffle/decoder";
 
 import { getProjectInfoForTransaction } from "./getProjectInfoForTransaction";
 import { serializeCalldataDecoding } from "./serializeCalldataDecoding";
+import { extractTransaction } from "./extractTransaction";
+import { prepareConfig } from "./prepareConfig";
 
 export const handleTransaction = async (
   request: express.Request,
   response: express.Response
 ): Promise<void> => {
-  // HACK add non-nullability to Transaction's `to` field; we'll only handle
-  // calls to existing contracts for now
-  const transaction: Transaction & { to: string } = {
-    // @ts-ignore cause hackathon
-    from: request.query.from,
-    // @ts-ignore cause hackathon
-    to: request.query.to,
-    // @ts-ignore cause hackathon
-    input: request.query.data,
-    blockNumber: null
-  };
-
-  const config = TruffleConfig.default().merge({
-    networks: {
-      mainnet: {
-        url: process.env["MAINNET_URL"],
-        network_id: 1
-      }
-    },
-    compilers: {
-      solc: {
-        docker: true
-      }
-    },
-    network: "mainnet",
-    // May need to make a more proper truffle config, will see. TODO
-    sourceFetchers: ["etherscan"],
-    etherscan: {
-      apiKey: process.env["ETHERSCAN_KEY"]
-    }
-  });
+  const transaction = await extractTransaction({ request });
+  const config = await prepareConfig();
 
   try {
     const projectInfo: ProjectInfo = await getProjectInfoForTransaction({
